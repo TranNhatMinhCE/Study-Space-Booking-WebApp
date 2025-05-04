89d66fbeee735c1e91370904a99eaa13c96c6d2b
trung0102 <trung.nguyen@hcmut.edu.vn>
2025-05-04 20:46:29 +0700

    Model Studyspace

--- BE/apps/resources/models.py
+++ BE/apps/resources/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class StudySpace(models.Model):
+    capacity = models.IntegerField()
+    space_type = models.CharField(
+        max_length=20,
+        choices= ["INDIVIDUAL", "GROUP", "MENTORING"],
+        default= "INDIVIDUAL"
+    )
+    space_status = models.CharField(
+        max_length=20,
+        choices= ["EMPTY", "BOOKED", "INUSE"],
+        default= "EMPTY"
+    )
+
+    def __str__(self):
+        return f"{self.capacity} - {self.space_type} - {self.space_status}"
